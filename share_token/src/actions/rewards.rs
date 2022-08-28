3f2ba7da842cdb8a0f5c9d5c1e6ab6854d135efa
Jasso <veigajoaoa@gmail.com>
2022-08-28 15:39:43 -0300

    test: Unit test rewards module

--- share_token/src/actions/rewards.rs
+++ share_token/src/actions/rewards.rs
@@ -11,6 +11,7 @@ use near_sdk::is_promise_success;
 impl Contract {
     #[payable]
     pub fn claim_rewards(&mut self) -> PromiseOrValue<bool> {
+        assert_one_yocto();
         let account_id = env::predecessor_account_id();
         let withdraw_value = self.withdraw_rewards(&account_id);
 
@@ -48,5 +49,148 @@ impl Contract {
 
         user_rps.update_rps(self.contract_rps.0, user_balance.0);
         user_rps.rewards_balance
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use near_sdk::PromiseResult;
+
+    use super::*;
+    use crate::tests::*;
+
+    #[rstest]
+    /// Test claim_rewards method
+    /// ASSERT:
+    /// (1) Call requires 1 yocto
+    #[should_panic(expected = "Requires attached deposit of exactly 1 yoctoNEAR")]
+    #[case(0, 0)]
+    /// (2) Changes caller internal reward balance to 0
+    /// (3) Emits promise with callback in case there are
+    ///     rewards to withdraw
+    #[case(1, 10)]
+    fn test_claim_rewards(#[case] deposit: u128, #[case] internal_balance: u128) {
+        // setup
+        let context = get_context(
+            vec![],
+            deposit,
+            0,
+            USER_ACCOUNT.parse().unwrap(),
+            0,
+            Gas(300u64 * 10u64.pow(12)),
+        );
+        testing_env!(context);
+        let user = USER_ACCOUNT.parse().unwrap();
+        let mut contract = init_contract(1);
+        register_user(&mut contract, &user, 100, internal_balance);
+
+        // call tested method
+        contract.claim_rewards();
+
+        // perform assertions
+        assert_eq!(
+            contract.accounts_rps.get(&user).unwrap().rewards_balance.0,
+            0
+        );
+
+        if internal_balance > 0 {
+            let receipts = get_created_receipts();
+            assert_eq!(receipts.len(), 2);
+
+            assert_eq!(receipts[0].receiver_id, contract.reward_token.clone());
+            assert_eq!(receipts[0].actions.len(), 1);
+
+            if let VmAction::FunctionCall {
+                function_name,
+                args,
+                gas: _,
+                deposit,
+            } = receipts[0].actions[0].clone()
+            {
+                assert_eq!(function_name, "ft_transfer");
+                assert_eq!(deposit, 1);
+                let json_args: serde_json::Value =
+                    serde_json::from_str(from_utf8(&args).unwrap()).unwrap();
+                assert_eq!(json_args["receiver_id"], user.to_string());
+                assert_eq!(json_args["amount"], internal_balance.to_string());
+            } else {
+                panic!()
+            };
+
+            assert_eq!(receipts[1].receiver_id, CONTRACT_ACCOUNT.parse().unwrap());
+            assert_eq!(receipts[1].actions.len(), 1);
+
+            if let VmAction::FunctionCall {
+                function_name,
+                args,
+                gas: _,
+                deposit,
+            } = receipts[1].actions[0].clone()
+            {
+                assert_eq!(function_name, "resolve_reward_transfer");
+                assert_eq!(deposit, 0);
+                let json_args: serde_json::Value =
+                    serde_json::from_str(from_utf8(&args).unwrap()).unwrap();
+                assert_eq!(json_args["receiver_id"], user.to_string());
+                assert_eq!(json_args["amount"], internal_balance.to_string());
+            } else {
+                panic!()
+            };
+        }
+    }
+
+    #[rstest]
+    /// Test resolve_reward_transfer method
+    /// ASSERT:
+    /// (1) Method only allows contract to call it
+    /// ***Assertion can't currently be made because of
+    ///    flaw in #[private] macro in mocked context
+    // #[should_panic(expected = "Method resolve_reward_transfer is private")]
+    // #[case(OWNER_ACCOUNT.parse().unwrap(), false, 100)]
+    /// (2) If promise succeeded does nothing
+    #[case(CONTRACT_ACCOUNT.parse().unwrap(), true, 100)]
+    /// (3) If promise fails resume user balance
+    #[case(CONTRACT_ACCOUNT.parse().unwrap(), false, 100)]
+    fn test_resolve_reward_transfer(
+        #[case] caller: AccountId,
+        #[case] promise_success: bool,
+        #[case] transferred_balance: u128,
+    ) {
+        // setup
+        let context = get_context(vec![], 0, 0, caller, 0, Gas(50u64 * 10u64.pow(12)));
+        
+        let promise_result = if promise_success {
+            PromiseResult::Successful(vec![])
+        } else {
+            PromiseResult::Failed
+        };
+        
+        testing_env!(
+            context,
+            VMConfig::test(),
+            RuntimeFeesConfig::test(),
+            HashMap::default(),
+            vec![promise_result]
+          );
+        let user = USER_ACCOUNT.parse().unwrap();
+        let mut contract = init_contract(1);
+        register_user(&mut contract, &user, 100, 0);
+
+        // call tested method
+        contract.resolve_reward_transfer(user.clone(), U128(transferred_balance));
+    
+        // make assertions
+        if promise_success {
+            assert_eq!(
+                contract.accounts_rps.get(&user).unwrap().rewards_balance.0,
+                0
+            );
+        } else {
+            assert_eq!(
+                contract.accounts_rps.get(&user).unwrap().rewards_balance.0,
+                transferred_balance
+            );
+        }
     }
 }
--- share_token/src/lib.rs
+++ share_token/src/lib.rs
@@ -24,8 +24,8 @@ use near_sdk::json_types::U128;
 #[allow(unused_imports)]
 use near_sdk::serde::{self, Deserialize, Serialize};
 use near_sdk::{
-    env, near_bindgen, utils::assert_one_yocto, AccountId, Balance, BorshStorageKey,
-    PanicOnDefault, Promise, PromiseOrValue, Gas, log
+    env, log, near_bindgen, utils::assert_one_yocto, AccountId, Balance, BorshStorageKey, Gas,
+    PanicOnDefault, Promise, PromiseOrValue,
 };
 
 use near_contract_standards;
@@ -52,8 +52,8 @@ use rps_manager::RpsManager;
 
 /// Exact byte size of data stored for each user that registers
 /// in the contract. Contract takes 154 bytes for information plus
-/// 2 bytes for each UTF8 char in account ID (LookupMaps in 
-/// ft_functionality and accounts_rps), which amounts to 
+/// 2 bytes for each UTF8 char in account ID (LookupMaps in
+/// ft_functionality and accounts_rps), which amounts to
 /// 282 bytes. Consider 300 to give a 10% wiggle room.
 const STORAGE_BYTES_PER_USER: u64 = 300;
 
@@ -225,7 +225,7 @@ impl Contract {
 
     /// Rolls back effects from withdraw_rewards. Is called in case the
     /// token transfer fails and the user's internal balance must be
-    /// reconstituted. 
+    /// reconstituted.
     pub fn rollback_withdraw_reward(&mut self, account_id: &AccountId, amount: u128) {
         let mut user_rps = self
             .accounts_rps
@@ -273,5 +273,163 @@ impl Contract {
             memo: None,
         }
         .emit();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    pub use near_sdk::collections::LazyOption;
+    pub use near_sdk::mock::VmAction;
+    pub use near_sdk::serde_json::{self, json};
+    pub use near_sdk::test_utils::{get_created_receipts, get_logs};
+    pub use near_sdk::{testing_env, Balance, Gas, MockedBlockchain, VMContext};
+    pub use near_sdk::{RuntimeFeesConfig, VMConfig};
+
+    pub use rstest::{fixture, rstest};
+
+    pub use std::collections::HashMap;
+    pub use std::convert::{TryFrom, TryInto};
+    pub use std::panic::{catch_unwind, UnwindSafe};
+    pub use std::str::from_utf8;
+
+    pub use super::*;
+
+    /// Mocked contract account id
+    pub const CONTRACT_ACCOUNT: &str = "contract.testnet";
+    /// Mocked rewards token account id
+    pub const REWARDS_TOKEN_ACCOUNT: &str = "rewards.testnet";
+    /// Mocked owner account id
+    pub const OWNER_ACCOUNT: &str = "owner.testnet";
+    /// Mocked regular user account id
+    pub const USER_ACCOUNT: &str = "user.testnet";
+    /// Total token supply to use in tests
+    pub const TOKEN_SUPPLY: U128 = U128(100000);
+
+    /// Initializes mocked blockchain context
+    pub fn get_context(
+        input: Vec<u8>,
+        attached_deposit: u128,
+        account_balance: u128,
+        signer_id: AccountId,
+        block_timestamp: u64,
+        prepaid_gas: Gas,
+    ) -> VMContext {
+        VMContext {
+            current_account_id: CONTRACT_ACCOUNT.parse().unwrap(),
+            signer_account_id: signer_id.clone(),
+            signer_account_pk: vec![0; 33].try_into().unwrap(),
+            predecessor_account_id: signer_id.clone(),
+            input,
+            block_index: 0,
+            block_timestamp,
+            account_balance,
+            account_locked_balance: 0,
+            storage_usage: 0,
+            attached_deposit,
+            prepaid_gas,
+            random_seed: [0; 32],
+            view_config: None,
+            output_data_receivers: vec![],
+            epoch_height: 19,
+        }
+    }
+
+    /// Initializes contract with random seed as storage keys to
+    /// guarantee no collisions
+    pub fn init_contract(seed: u128) -> Contract {
+        let hash1 = env::keccak256(&seed.to_be_bytes());
+        let hash2 = env::keccak256(&hash1[..]);
+        let hash3 = env::keccak256(&hash2[..]);
+        let hash4 = env::keccak256(&hash3[..]);
+        let hash5 = env::keccak256(&hash4[..]);
+        let token_metadata = FungibleTokenMetadata {
+            spec: "ft-1.0.0".to_string(),
+            name: format!("{}-token", "token_name"),
+            symbol: "token_symbol".to_string(),
+            icon: Some("token_icon".to_string()),
+            reference: Some("token_reference".to_string()),
+            reference_hash: None,
+            decimals: 0,
+        };
+        let nft_contract_metadata = NFTContractMetadata {
+            spec: "nft-1.0.0".to_string(),
+            name: format!("{}-token", "token_name"),
+            symbol: "token_symbol".to_string(),
+            icon: Some("token_icon".to_string()),
+            base_uri: None,
+            reference: Some("token_reference".to_string()),
+            reference_hash: None,
+        };
+        let nft_instance_metadata = NftInstanceData {
+            title: "nft_instance_name".to_string(),
+            description: "nft_instance_description".to_string(),
+            media: "nft_instance_media_url".to_string(),
+            reference: "token_reference".to_string(),
+        };
+        let mut this = Contract {
+            ft_functionality: FungibleToken::new(hash1),
+            reward_token: REWARDS_TOKEN_ACCOUNT.parse().unwrap(),
+            reward_tokens_all_time_count: U128(0),
+            contract_rps: U128(0),
+            accounts_rps: LookupMap::new(hash2),
+            token_metadata: LazyOption::new(hash3, Some(&token_metadata)),
+            nft_contract_metadata: LazyOption::new(hash4, Some(&nft_contract_metadata)),
+            nft_instance_metadata: LazyOption::new(hash5, Some(&nft_instance_metadata)),
+        };
+        this.update_user_rps(&OWNER_ACCOUNT.parse().unwrap());
+        this.ft_functionality
+            .internal_register_account(&OWNER_ACCOUNT.parse().unwrap());
+        this.ft_functionality
+            .internal_deposit(&OWNER_ACCOUNT.parse().unwrap(), TOKEN_SUPPLY.into());
+        this
+    }
+
+    pub fn register_user(
+        contract: &mut Contract,
+        user: &AccountId,
+        token_balance: u128,
+        rewards_balance: u128,
+    ) {
+        contract.update_user_rps(user);
+        contract.ft_functionality.internal_register_account(user);
+        contract
+            .ft_functionality
+            .internal_deposit(user, token_balance);
+        let mut internal_rps = contract.accounts_rps.get(user).unwrap();
+        internal_rps.rewards_balance = U128(rewards_balance);
+        contract.accounts_rps.insert(user, &internal_rps);
+    }
+
+    #[rstest]
+    fn test_new() {
+        let context = get_context(
+            vec![],
+            0,
+            0,
+            OWNER_ACCOUNT.parse().unwrap(),
+            0,
+            Gas(300u64 * 10u64.pow(12)),
+        );
+        testing_env!(context);
+
+        let contract = Contract::new(
+            OWNER_ACCOUNT.parse().unwrap(),
+            TOKEN_SUPPLY,
+            REWARDS_TOKEN_ACCOUNT.parse().unwrap(),
+            "NAME_HERE".to_string(),
+            "SYMBOL_HERE".to_string(),
+            Some("ICON_HERE".to_string()),
+            "REFERENCE_HERE".to_string(),
+            "NFT_NAME_HERE".to_string(),
+            "NFT_DESC_HERE".to_string(),
+            "NFT_MEDIA_HERE".to_string(),
+        );
+
+        assert_eq!(
+            TOKEN_SUPPLY,
+            contract
+                .ft_functionality
+                .ft_balance_of(OWNER_ACCOUNT.parse().unwrap())
+        );
     }
 }
